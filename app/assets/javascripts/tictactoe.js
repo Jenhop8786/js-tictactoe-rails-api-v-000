e054b40e1c6323e770a5e9ab5baac8c99d2cf873
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-09-26 17:00:54 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -3,9 +3,17 @@
 var turn = 0;
 var gameId = 0;
 var winCombinations= [
-    [0,1,2], /*top row*/ [3,4,5], /* middle row*/ [6,7,8], /*bottom row*/
-    [0,3,6], /*1st col*/ [1,4,7], /*2nd col*/ [2,5,8], /*3rd col*/
-    [0,4,8], /*neg diag*/ [2,4,6] /*pos diag*/
+    [0,1,2], 
+    [3,4,5], 
+    [6,7,8], 
+    
+    [0,3,6], 
+    [1,4,7], 
+    [2,5,8], 
+    
+    [0,4,8], 
+    [2,4,6] 
+    
 ];
 
 function player () {
